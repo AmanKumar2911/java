fbcb5e16a6142e98882fb5712e180cdfa38c55b5
AmanKumar2911 <ang22911@gmail.com>
2023-02-26 14:19:24 +0530

    commmit for practice

--- practice2.java
+++ practice2.java
@@ -1,23 +1,24 @@
 import java.util.Arrays;
-// https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/
-public class practice {
+
+// https://leetcode.com/problems/first-missing-positive/
+
+public class practice2 {
     public static void main(String[] args) {
-        int[]arr=new int[8];
-        arr[0]=4;arr[1]=3;arr[2]=2;arr[3]=7;arr[4]=8;arr[5]=2;arr[6]=3;arr[7]=1;
+        int[] arr={3,4,-1,1};
         cyclic(arr);
         System.out.println(Arrays.toString(arr));
         for(int i=0;i<arr.length;i++){
-            if(arr[i]!=i+1){
-                System.out.println(i+1);
+            if(arr[i] < arr.length && arr[i]!=i){
+                System.out.println(i);
             }
         }
-
+        // System.out.println(arr.length);
     }
     static void cyclic(int[]arr){
         int i=0;
         while(i<arr.length){
-           int correct=arr[i]-1;
-           if(arr[i]!=arr[correct]){
+           int correct=arr[i];
+           if(arr[i]>0 && arr[i] < arr.length && arr[i]!=arr[correct]){
             swap(arr,i,correct);
            }
            else{
---
+++ strings_stringbuilder.java
@@ -0,0 +1,11 @@
+public class strings_stringbuilder {
+    public static void main(String[] args) {
+        String str="Aman";
+        String st="Aman";
+        str="ak";
+        System.out.println(str);
+        System.out.println(st);
+
+    }
+    
+}
